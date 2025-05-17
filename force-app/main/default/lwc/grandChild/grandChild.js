a623804225fe5fa38b4f5af1f279187afa8cafdd
TRANSENIGMA21 <call.ajay21@gmail.com>
2025-05-18 02:30:00 +0530

    commit

--- force-app/main/default/lwc/grandChild/grandChild.js
+++ force-app/main/default/lwc/grandChild/grandChild.js
@@ -1,12 +1,11 @@
 import { LightningElement, track, wire } from 'lwc';
-import getAccount from '@salesforce/apex/Demo.getAccount';
 import getString from '@salesforce/apex/Demo.getString';
 
 export default class GrandChild extends LightningElement {
     @track grandchild = 'GrandChild';
     result
 
-    @wire(getAccount) accounts;
+   
     @wire(getString) stringtest;
     connectedCallback() {
         console.log('in grandchild');
--- force-app/main/default/lwc/grantParent/grantParent.js
+++ force-app/main/default/lwc/grantParent/grantParent.js
@@ -1,7 +1,8 @@
-import { LightningElement, track } from 'lwc';
-
+import { LightningElement, track, wire } from 'lwc';
+import getAccount from '@salesforce/apex/Demo.getAccount';
 export default class GrantParent extends LightningElement {
     @track grandParent = 'GrandParent';
+    @wire(getAccount) accounts;
 
     connectedCallback() {
         console.log('in grandParent');        
@@ -10,4 +11,6 @@ export default class GrantParent extends LightningElement {
     renderedCallback() {
         console.log('in grandparent renderedCallback');
     }
+
+    
 }
